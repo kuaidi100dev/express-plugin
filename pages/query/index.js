00c137b054d407cc9002a3fdee6ccaf2a2685be3
kuaidi100 <dev@kuaidi100.com>
2021-10-12 16:21:39 +0800

    demo

---
+++ pages/query/index.js
@@ -0,0 +1,80 @@
+// pages/plugins/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  go(e) {
+    const target = e.currentTarget.dataset
+    wx.navigateTo({
+      url: `plugin://expressPlugin/index?num=${target.num}&com=${target.com || ""}&appName=快乐购物&title=物流详情`,
+    })
+    
+
+    // wx.navigateToMiniProgram({
+    //   appId: "wx6885acbedba59c14",
+    //   path: `pages/result/result?nu=${target.num}&com=${target.com}&querysource=third_xcx`
+    // })
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
